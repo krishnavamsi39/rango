175a6a522f5db7fa8e60109503400b1411071693
krishnavamsi39 <37270971+krishnavamsi39@users.noreply.github.com>
2018-07-04 16:08:33 +0530

    Add files via upload

---
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+// JQuery code to be added in here.
+$("#about-btn").click( function(event) {
+alert("You clicked me! ouch!");
+});
+$("#about-btn").addClass('btn btn-primary');
+});
